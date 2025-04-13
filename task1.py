c646753dcbcdabcf421e01f8055736a488c38699
DaZorig <98804139+DaZorig@users.noreply.github.com>
2025-04-13 10:56:08 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -22,6 +22,43 @@ def right_triangle(side_one, side_two, side_three):
     return hypotenuse == math.sqrt(all_sides[0]**2 + all_sides[1]**2)
 
 
+# Вычислим площадь фигуры без знания типа фигуры
+# Вопрос в том, какие данные известные у фигуры ?
+# Допустим, известны все стороны и углы между ними
+
+
+# Рассмотрим фигуры с 4-мя сторонами, необходимо написать проверку
+# на тип фигуры (квадрат, прямоугольник,  ромб, параллелограм
+def define_figure(side_one, side_two, side_three, side_four, angle_one, angle_two, angle_three, angle_four):
+    if side_one == side_two and side_one == side_three and side_one == side_four:
+        if angle_one == angle_two and angle_one == angle_three and angle_one == angle_four:
+            return side_one**2  # Если все стороны и углы равны, то это фигура - квадрат, площадь = a^2
+        else:  # Если не все углы равны, то эта фигура ромб, необходимо найти высоту для вычисления площади
+            all_angles = [angle_one, angle_two, angle_three, angle_four]  # Добавим все углы в массив
+            min_angle = min(all_angles)  # Найдем меньший из углов
+            height = side_one * math.sin(min_angle * math.pi / 180)  # Т.к. min_angle - острый угол, найдем высоту ромба
+            return side_one * height  # Вычислим площадь ромба как произведение стороны на высоту
+    else:  # Проверим 4-угольник на тип фигуры (прямоугольник, параллелограм)
+        all_sides = [side_one, side_two, side_three, side_four]
+        min_side = min(all_sides)
+        max_side = max(all_sides)
+        all_sides.remove(min_side)
+        all_sides.remove(max_side)
+        flag = 0  # Флаг используется как проверка на то, чтобы было две пары одинаковых сторон
+        for side in all_sides:
+            if side == min_side or side == max_side:
+                flag += 1
+        if flag == 2:  # Эта фигура прямоугольник или параллелограм
+            if angle_one == angle_two and angle_one == angle_three and angle_one == angle_four:
+                return min_side * max_side  # Если все углы равны, то это фигура - прямоугольик, площадь = a*b
+            else:  # Если не все углы равны, то эта фигура параллелограм, необходимо найти высоту для вычисления площади
+                all_angles = [angle_one, angle_two, angle_three, angle_four]  # Добавим все углы в массив
+                min_angle = min(all_angles)  # Найдем меньший из углов
+                height = min_side * math.sin(
+                    min_angle * math.pi / 180)  # Т.к. min_angle - острый угол, найдем высоту параллеограма
+                return max_side * height  # Вычислим площадь параллелограма как произведение стороны на высоту
+
+
 # Напишем юнит-тесты для проверки определенных функций
 # Импортируем библиотеку unittest
 import unittest
@@ -37,9 +74,16 @@ class TestFunctions(unittest.TestCase):
         self.assertAlmostEqual(triangle_area(6, 7, 8), 20.33, places=2)
 
 
-    def test_right_triangle(self): # Проверка определения прямоугольного прямоугольника по трем сторонам
+    def test_right_triangle(self):  # Проверка определения прямоугольного прямоугольника по трем сторонам
         self.assertTrue(right_triangle(5, 12, 13))
         self.assertFalse(right_triangle(5, 6, 10))
+
+
+    def test_define_figure(self):  # Проверка вычисления площади частных случаев четырехугольников
+        self.assertAlmostEqual(define_figure(4, 4, 4, 4, 90, 90, 90, 90), 16)  # Квадрат
+        self.assertAlmostEqual(define_figure(5, 5, 5, 5, 120, 60, 60, 120), 21.65, places=2)  # Ромб
+        self.assertAlmostEqual(define_figure(4, 4, 8, 8, 90, 90, 90, 90), 32)  # Прямоугольник
+        self.assertAlmostEqual(define_figure(5, 7, 7, 5, 120, 60, 60, 120), 30.31, places=2)  # Параллелограм
 
 
 if __name__ == '__main__':
